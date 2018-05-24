d6f902291b1e5630c0ec0b362c853e1e575cb1b5
didi <didimilikina8@gmail.com>
2018-05-24 16:14:06 +0300

    Added html, images,css.

--- src/app/figure/figure-landing-page/figure-landing-page.component.ts
+++ src/app/figure/figure-landing-page/figure-landing-page.component.ts
@@ -1,12 +1,42 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChildren, AfterViewInit } from '@angular/core';
 
 @Component({
   selector: 'app-figure-landing-page',
   templateUrl: './figure-landing-page.component.html',
   styleUrls: ['./figure-landing-page.component.css']
 })
-export class FigureLandingPageComponent {
+export class FigureLandingPageComponent implements AfterViewInit {
 
-  constructor() { }
+  private slideIndex = 1;
 
+  @ViewChildren('mySlides')
+  private elements;
+
+  constructor() {
+  }
+  ngAfterViewInit(): void {
+    this.showDivs(this.slideIndex);
+  }
+
+  public plusDivs(n) {
+    this.showDivs(this.slideIndex += n);
+  }
+
+  public showDivs(n) {
+    // let i;
+    // debugger
+    // if (n > this.elements.length) {
+    //   this.slideIndex = 1
+    // }
+    // if (n < 1) {
+    //   this.slideIndex = this.elements.length
+    // }
+    // this.elements.forEach(element => {
+    //   debugger
+    //   element.nativeElement.style.display = "none";
+    // });
+    // if (this.elements[this.slideIndex - 1] != null) {
+    //   this.elements[this.slideIndex - 1].nativeElement.style.display = "block";
+    // }
+  }
 }
